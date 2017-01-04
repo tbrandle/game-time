9174b567e128e1c57b9372de4ea5c403ad2dc83e
Tim <tbrandle53@gmail.com>
2017-01-04 13:25:40 -0700

    Begin testing for ball-test.js

--- test/ball-test.js
+++ test/ball-test.js
@@ -1,9 +1,26 @@
 var assert = require('chai').assert;
 var Ball = require('../lib/ball');
 
-describe('Ball is a constructor function', function () {
+describe('Ball', function () {
 
   it('should create a ball object', function () {
     assert.isFunction(Ball);
   });
 });
+
+describe('ball prototypes' function () {
+
+  it.skip('should have a method called "drawBall()"', function () {
+
+  })
+  it.skip('should have a method called "drawBall()"', function () {
+
+  })
+  it.skip('should have a method called "drawBall()"', function () {
+
+  })
+  it.skip('should have a method called "drawBall()"', function () {
+    
+  })
+
+})
