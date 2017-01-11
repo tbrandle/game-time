e0a77dda992d1a77593372f58acd0c41a4117b4c
Charles Dunn <dunnc1551@gmail.com>
2017-01-11 13:53:45 -0700

    Add tests for levels

--- test/levels-test.js
+++ test/levels-test.js
@@ -1,4 +1,51 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const Block = require('../lib/block');
+const Game = require('../lib/levels');
+const Ball = require('../lib/ball');
+const Paddle = require('../lib/paddle');
+
+
+describe('Levels', function() {
+
+  it('should create an object called Game', function() {
+    assert.isFunction(Game);
+  });
+
+  it('should have an empty array of blocks by default', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    assert.deepEqual(game.blocks, []);
+  });
+
+  it('should have a default level of 0', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    assert.equal(game.level, 0);
+  });
+
+  it('should instantiate a new ball', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    assert.isObject(game.ball, new Ball({}))
+  })
+
+  it('should instantiate a new paddle', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    assert.isObject(game.paddle, new Paddle({}))
+  });
+
+  it('should have a method called createRow', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    assert.isFunction(game.createRow);
+  });
+
+  it('should take an object as an argument', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    assert.equal(game.createRow({}));
+  });
+
+  it('should give block a default x position', function() {
+    var game = new Game({context: {}, canvas: {width: 400}});
+    game.createRow({block});
+    assert.equal(game.xStart, 150);
+  })
+
+})
