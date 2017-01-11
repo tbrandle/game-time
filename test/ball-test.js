92b3b52c66e101e0766c0a156bc123410d0f7660
Charles Dunn <dunnc1551@gmail.com>
2017-01-10 22:00:22 -0700

    Add passing tests for ball, block, and paddle constructor functions

--- test/ball-test.js
+++ test/ball-test.js
@@ -8,7 +8,62 @@ describe('Ball', function () {
   it('should create a ball object', function () {
     assert.isFunction(Ball);
   });
-});
+
+  it('should have a default x position', function() {
+    var ball = new Ball({context: {}, canvas:{width: 400}});
+    assert.equal(ball.x, 200);
+  });
+
+  it('should have a default y position', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.y, 260);
+  });
+
+  it('should have a radius of 6', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.radius, 6);
+  });
+
+  it('should have a start angle of 0', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.startAngle, 0);
+  });
+
+  it('should have an end angle of 6.283', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.endAngle, (2 * Math.PI));
+  });
+
+  it('should have a default speed of 3', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.speed, 3);
+  });
+
+  it('should have a lateral speed of 3', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.dx, 3);
+  });
+
+  it('should have a vertical speed of -3', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.dy, -3);
+  });
+
+  it('should start with three lives', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.lives, 3);
+  });
+
+  it('should start with a game score of 0', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.gameScore, 0);
+  });
+
+  it('should have a default color of black', function() {
+    var ball = new Ball({context: {}, canvas:{}});
+    assert.equal(ball.color, '#000000');
+  })
+})
 
 describe('ball prototypes', function () {
   var ball = new Ball({context: {}, canvas:{width: 400, height: 300}});
--- test/paddle-test.js
+++ test/paddle-test.js
@@ -3,13 +3,35 @@ const assert = chai.assert;
 
 const Paddle = require('../lib/paddle');
 
-describe('paddle object contructor', function() {
+describe('Paddle', function() {
 
   it('create paddle', function() {
     assert.isFunction(Paddle);
   });
-  it('should have default y position', function() {
+
+  it('should have a default x position', function() {
+    var paddle = new Paddle({context: {}, canvas: {width: 400}});
+    assert.equal(paddle.x, 150);
+  });
+
+  it('should have a default y position', function() {
     var paddle = new Paddle({context: {}, canvas: {width: 400}});
     assert.equal(paddle.y, 275);
+  });
+
+  it('should have a default width', function() {
+    var paddle = new Paddle({context: {}, canvas: {}});
+    assert.equal(paddle.width, 100);
+  });
+
+  it('should have a default height', function() {
+    var paddle = new Paddle({context: {}, canvas: {}});
+    assert.equal(paddle.height, 10);
+  });
+
+  it('should have a default color', function() {
+    var paddle = new Paddle({context: {}, canvas: {}});
+    assert.equal(paddle.color, '#91c09e');
   })
+
 });
